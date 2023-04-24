f6d5e736b9b2d61772e2edb2514741122d52c0d7
Sara99 <sara.acosta01@uptc.edu.co>
2023-04-24 13:53:39 -0500

    New Component: MyBoton

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { MyBoton } from './components/MyBoton/MyBoton';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <MyBoton/> 
+  )
 }
-
-export default App;
+export default App;//->exporta archivo con un solo componente
+//funcion
+//nombre funcion = nobre componente
+//primera en mayuscula, componente funcional
+//export:componente para todo el proyecto 
